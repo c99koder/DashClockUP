ddb1ddb21e8fb01c740a02a7de337208c819be44
Sam Steele <sam.steele@gmail.com>
2014-10-04 10:54:05 -0700

    Initial checkin

---
+++ app/src/main/java/org/c99/dashclockup/DashclockExtension.java
@@ -0,0 +1,165 @@
+package org.c99.dashclockup;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.google.android.apps.dashclock.api.DashClockExtension;
+import com.google.android.apps.dashclock.api.ExtensionData;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.jawbone.upplatformsdk.api.ApiManager;
+import com.jawbone.upplatformsdk.api.response.OauthAccessTokenResponse;
+import com.jawbone.upplatformsdk.utils.UpPlatformSdkConstants;
+
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+/**
+ * Created by sam on 10/4/14.
+ */
+public class DashclockExtension extends DashClockExtension {
+    public final static String REFRESH_INTENT = "org.c99.DashClockUP.REFRESH";
+    RefreshReceiver receiver;
+
+    class RefreshReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            onUpdateData(0);
+        }
+    }
+
+    @Override
+    protected void onInitialize(boolean isReconnect) {
+        super.onInitialize(isReconnect);
+        if (receiver != null)
+            try {
+                unregisterReceiver(receiver);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        IntentFilter intentFilter = new IntentFilter(REFRESH_INTENT);
+        receiver = new RefreshReceiver();
+        registerReceiver(receiver, intentFilter);
+
+        setUpdateWhenScreenOn(true);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (receiver != null)
+            try {
+                unregisterReceiver(receiver);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+    }
+
+    @Override
+    protected void onUpdateData(int reason) {
+        if(PreferenceManager.getDefaultSharedPreferences(this).contains(UpPlatformSdkConstants.UP_PLATFORM_ACCESS_TOKEN)) {
+            ApiManager.getRequestInterceptor().setAccessToken(PreferenceManager.getDefaultSharedPreferences(this).getString(UpPlatformSdkConstants.UP_PLATFORM_ACCESS_TOKEN, ""));
+            ApiManager.getRestApiInterface().getUser(UpPlatformSdkConstants.API_VERSION_STRING, getUserRequestListener);
+            ApiManager.getRestApiInterface().getUsersGoals(UpPlatformSdkConstants.API_VERSION_STRING, getGoalRequestListener);
+            if(PreferenceManager.getDefaultSharedPreferences(this).contains("steps") && PreferenceManager.getDefaultSharedPreferences(this).contains("goal"))
+                update();
+        } else {
+            Intent i = new Intent(this, SettingsActivity.class);
+            i.putExtra("doLogin", true);
+            publishUpdate(new ExtensionData()
+                            .visible(true)
+                            .icon(R.drawable.ic_up)
+                            .status("?")
+                            .expandedTitle("Not logged in")
+                            .expandedBody("Tap to login to Jawbone UP")
+                            .clickIntent(i)
+            );
+        }
+    }
+
+    private void update() {
+        if(PreferenceManager.getDefaultSharedPreferences(this).contains("steps") && PreferenceManager.getDefaultSharedPreferences(this).contains("goal")) {
+            int steps = PreferenceManager.getDefaultSharedPreferences(this).getInt("steps", 0);
+            int goal = PreferenceManager.getDefaultSharedPreferences(this).getInt("goal", 0);
+            publishUpdate(new ExtensionData()
+                            .visible(true)
+                            .icon(R.drawable.ic_up)
+                            .status(NumberFormat.getInstance().format(steps))
+                            .expandedTitle(NumberFormat.getInstance().format(steps) + " Steps Today")
+                            .expandedBody((int) (((float) steps / (float) goal) * 100.0) + "% of your " + NumberFormat.getInstance().format(goal) + " step goal")
+            );
+        }
+    }
+
+    private Callback getGoalRequestListener = new Callback<JsonObject>() {
+        @Override
+        public void success(JsonObject result, Response response) {
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(DashclockExtension.this);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt("goal", result.get("data").getAsJsonObject().get("move_steps").getAsInt());
+            editor.commit();
+            ApiManager.getRestApiInterface().getMoveEventsList(UpPlatformSdkConstants.API_VERSION_STRING, null, getMovesRequestListener);
+        }
+
+        @Override
+        public void failure(RetrofitError retrofitError) {
+            Log.e("DashClockUP", "failed to get user:" + retrofitError.getMessage());
+        }
+    };
+
+    private Callback getMovesRequestListener = new Callback<JsonObject>() {
+        @Override
+        public void success(JsonObject result, Response response) {
+            JsonArray items = result.get("data").getAsJsonObject().get("items").getAsJsonArray();
+            if(items != null && items.size() > 0) {
+                int steps = 0;
+                JsonObject item = items.get(0).getAsJsonObject();
+                SimpleDateFormat f = new SimpleDateFormat("yyyyMMdd");
+                if(item.get("date").getAsString().equals(f.format(new Date()))) {
+                    steps = item.get("details").getAsJsonObject().get("steps").getAsInt();
+                }
+
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(DashclockExtension.this);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putInt("steps", steps);
+                editor.commit();
+            }
+            update();
+        }
+
+        @Override
+        public void failure(RetrofitError retrofitError) {
+            Log.e("DashClockUP", "failed to get user:" + retrofitError.getMessage());
+        }
+    };
+
+    private Callback getUserRequestListener = new Callback<JsonObject>() {
+        @Override
+        public void success(JsonObject result, Response response) {
+            JsonObject data = result.get("data").getAsJsonObject();
+            String name = data.get("first").getAsString();
+            if (data.has("last") && data.get("last").getAsString().length() > 0)
+                name += " " + data.get("last").getAsString();
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(DashclockExtension.this);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString("name", name);
+            editor.commit();
+        }
+
+        @Override
+        public void failure(RetrofitError retrofitError) {
+            Log.e("DashClockUP", "failed to get user:" + retrofitError.getMessage());
+        }
+    };
+}
---
+++ app/src/main/java/org/c99/dashclockup/SettingsActivity.java
@@ -0,0 +1,167 @@
+package org.c99.dashclockup;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.MenuItem;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.jawbone.upplatformsdk.api.ApiManager;
+import com.jawbone.upplatformsdk.api.response.OauthAccessTokenResponse;
+import com.jawbone.upplatformsdk.datamodel.Data;
+import com.jawbone.upplatformsdk.datamodel.Meta;
+import com.jawbone.upplatformsdk.oauth.OauthUtils;
+import com.jawbone.upplatformsdk.oauth.OauthWebViewActivity;
+import com.jawbone.upplatformsdk.utils.UpPlatformSdkConstants;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by sam on 10/4/14.
+ */
+public class SettingsActivity extends PreferenceActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActionBar().setIcon(R.drawable.ic_launcher);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        addPreferencesFromResource(R.xml.settings);
+
+        findPreference("login").setOnPreferenceClickListener(loginClickListener);
+    }
+
+    Preference.OnPreferenceClickListener loginClickListener = new Preference.OnPreferenceClickListener() {
+        @Override
+        public boolean onPreferenceClick(Preference preference) {
+            ApiManager.getRequestInterceptor().clearAccessToken();
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(SettingsActivity.this);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.remove(UpPlatformSdkConstants.UP_PLATFORM_REFRESH_TOKEN);
+            editor.remove(UpPlatformSdkConstants.UP_PLATFORM_ACCESS_TOKEN);
+            editor.remove("name");
+            editor.remove("steps");
+            editor.remove("goal");
+            editor.commit();
+
+            sendBroadcast(new Intent(DashclockExtension.REFRESH_INTENT));
+
+            CookieSyncManager.createInstance(SettingsActivity.this);
+            CookieManager cookieManager = CookieManager.getInstance();
+            cookieManager.removeAllCookie();
+            cookieManager.setAcceptCookie(true);
+
+            List<UpPlatformSdkConstants.UpPlatformAuthScope> authScope = new ArrayList<UpPlatformSdkConstants.UpPlatformAuthScope>();
+            authScope.add(UpPlatformSdkConstants.UpPlatformAuthScope.BASIC_READ);
+            authScope.add(UpPlatformSdkConstants.UpPlatformAuthScope.MOVE_READ);
+            Uri.Builder builder = OauthUtils.setOauthParameters(BuildConfig.CLIENT_ID, "http://localhost/dashclockup?", authScope);
+
+            Intent intent = new Intent(OauthWebViewActivity.class.getName());
+            intent.putExtra(UpPlatformSdkConstants.AUTH_URI, builder.build());
+            startActivityForResult(intent, UpPlatformSdkConstants.JAWBONE_AUTHORIZE_REQUEST_CODE);
+            return false;
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(PreferenceManager.getDefaultSharedPreferences(SettingsActivity.this).contains("name"))
+            findPreference("login").setSummary(PreferenceManager.getDefaultSharedPreferences(SettingsActivity.this).getString("name",""));
+        else if(!PreferenceManager.getDefaultSharedPreferences(SettingsActivity.this).contains(UpPlatformSdkConstants.UP_PLATFORM_ACCESS_TOKEN))
+            findPreference("login").setSummary("Tap to login");
+        else
+            findPreference("login").setSummary("");
+
+        if(getIntent() != null && getIntent().hasExtra("doLogin")) {
+            setIntent(new Intent(SettingsActivity.this, SettingsActivity.class));
+            loginClickListener.onPreferenceClick(null);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == UpPlatformSdkConstants.JAWBONE_AUTHORIZE_REQUEST_CODE && resultCode == RESULT_OK) {
+
+            String code = data.getStringExtra(UpPlatformSdkConstants.ACCESS_CODE);
+            if (code != null) {
+                ApiManager.getRequestInterceptor().clearAccessToken();
+                ApiManager.getRestApiInterface().getAccessToken(
+                        BuildConfig.CLIENT_ID,
+                        BuildConfig.CLIENT_SECRET,
+                        code,
+                        accessTokenRequestListener);
+            }
+        }
+    }
+
+    private Callback getUserRequestListener = new Callback<JsonObject>() {
+        @Override
+        public void success(JsonObject result, Response response) {
+            JsonObject data = result.get("data").getAsJsonObject();
+            String name = data.get("first").getAsString();
+            if(data.has("last") && data.get("last").getAsString().length() > 0)
+                name += " " + data.get("last").getAsString();
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(SettingsActivity.this);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString("name", name);
+            editor.commit();
+
+            findPreference("login").setSummary(name);
+            sendBroadcast(new Intent(DashclockExtension.REFRESH_INTENT));
+        }
+
+        @Override
+        public void failure(RetrofitError retrofitError) {
+            Log.e("DashClockUP", "failed to get user:" + retrofitError.getMessage());
+        }
+    };
+
+    private Callback accessTokenRequestListener = new Callback<OauthAccessTokenResponse>() {
+        @Override
+        public void success(OauthAccessTokenResponse result, Response response) {
+
+            if (result.access_token != null) {
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(SettingsActivity.this);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString(UpPlatformSdkConstants.UP_PLATFORM_ACCESS_TOKEN, result.access_token);
+                editor.putString(UpPlatformSdkConstants.UP_PLATFORM_REFRESH_TOKEN, result.refresh_token);
+                editor.commit();
+
+                ApiManager.getRequestInterceptor().setAccessToken(result.access_token);
+                ApiManager.getRestApiInterface().getUser(UpPlatformSdkConstants.API_VERSION_STRING, getUserRequestListener);
+            } else {
+                Log.e("DashClockUP", "accessToken not returned by Oauth call, exiting...");
+            }
+        }
+
+        @Override
+        public void failure(RetrofitError retrofitError) {
+            Log.e("DashClockUP", "failed to get accessToken:" + retrofitError.getMessage());
+        }
+    };
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
